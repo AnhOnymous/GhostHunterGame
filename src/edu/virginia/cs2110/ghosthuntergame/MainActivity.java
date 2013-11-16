449df1cf61322a5d5d2df2261dec9ae0398ece6d
AnhOnymous <minhanhonymous@gmail.com>
2013-11-16 16:05:14 -0500

    reverting back to before messing things up

--- src/edu/virginia/cs2110/ghosthuntergame/MainActivity.java
+++ src/edu/virginia/cs2110/ghosthuntergame/MainActivity.java
@@ -1,9 +1,7 @@
 package edu.virginia.cs2110.ghosthuntergame;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
-import android.app.Activity;
 
 public class MainActivity extends Activity {
 
@@ -12,12 +10,4 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
 }
