ce8bb7f618a7c7bb0f06f25f3274ba926dc5e66b
ConradTind <cst2zu@virginia.edu>
2013-11-19 17:44:45 -0500

    correct the random bone generator

--- src/edu/virginia/cs2110/ghosthuntergame/Bones.java
+++ src/edu/virginia/cs2110/ghosthuntergame/Bones.java
@@ -10,23 +10,13 @@ public class Bones {
 	private double teatherRadius;
 
 	public Bones(double userLongitude, double userLatitude,
-			double startLongitude, double startLatitude, double userRadius) {
+			double startLongitude, double startLatitude, double userRadius, double endLongitude, double endLatitude) {
 		// User longitude/latitude is the point at which the user starts in the
 		// game play
 		// NOTE: assumed that the player is centered int the map
 		// start long/lat is the top left corner of the gameplay field
 
-		// these methods must calculate the distance between the internal
-		// boundry and the external boundry, respecting whether they are
-		// lat/long
-		double left = calculateLeftBound(userLongitude, startLongitude,
-				userRadius);
-		double right = calculateRightBound(userLongitude, startLongitude,
-				userRadius);
-		double upper = calculateUpperBound(userLatitude, startLatitude,
-				userRadius);
-		double lower = calculateLowerBound(userLatitude, startLatitude,
-				userRadius);
+		
 
 		Random generator = new Random();
 
@@ -35,46 +25,73 @@ public class Bones {
 		switch (sectionNum) {
 		case 0:
 			sectionNum = 0;
-			longitude = startLongitude + generator.nextInt((int) left + 1);
-			latitude = startLatitude + generator.nextInt((int) upper + 1);
+			longitude = startLongitude + generator.nextInt((int) calculateLeftBound(userLongitude, startLongitude,
+				userRadius) + 1);
+			latitude = startLatitude + generator.nextInt((int) calculateUpperBound(userLatitude, startLatitude,
+				userRadius) + 1);
 		case 1:
 			sectionNum = 1;
-			longitude = startLongitude + generator.nextInt((int) left + 1)
-					+ left;
-			latitude = startLatitude + generator.nextInt((int) upper + 1);
+			longitude = startLongitude + generator.nextInt((int) calculateLeftBound(userLongitude, startLongitude,
+				userRadius) + 1)
+					+ calculateLeftBound(userLongitude, startLongitude,
+				userRadius);
+			latitude = startLatitude + generator.nextInt((int) calculateUpperBound(userLatitude, startLatitude,
+				userRadius) + 1);
 		case 2:
 			sectionNum = 2;
-			longitude = startLongitude + generator.nextInt((int) left + 1)
-					+ right;
-			latitude = startLatitude + generator.nextInt((int) upper + 1);
+			longitude = startLongitude + generator.nextInt((int) calculateLeftBound(userLongitude, startLongitude,
+				userRadius) + 1)
+					+ calculateRightBound(userLongitude, endLongitude,
+				userRadius);
+			//could use end latitude
+			latitude = startLatitude + generator.nextInt((int) calculateUpperBound(userLatitude, startLatitude,
+				userRadius) + 1);
 		case 3:
 			sectionNum = 3;
-			longitude = startLongitude + generator.nextInt((int) left + 1);
-			latitude = startLatitude + generator.nextInt((int) upper + 1)
-					+ upper;
+			longitude = startLongitude + generator.nextInt((int) calculateLeftBound(userLongitude, startLongitude,
+				userRadius) + 1);
+			latitude = startLatitude + generator.nextInt((int) calculateUpperBound(userLatitude, startLatitude,
+				userRadius) + 1)
+					+ calculateUpperBound(userLatitude, startLatitude,
+				userRadius);
 		case 4:
 			sectionNum = 4;
-			longitude = startLongitude + generator.nextInt((int) left + 1)
-					+ right;
-			latitude = startLatitude + generator.nextInt((int) upper + 1)
-					+ upper;
+			longitude = startLongitude + generator.nextInt((int) calculateLeftBound(userLongitude, startLongitude,
+				userRadius) + 1)
+					+ calculateRightBound(userLongitude, endLongitude,
+				userRadius);
+			latitude = startLatitude + generator.nextInt((int) calculateUpperBound(userLatitude, startLatitude,
+				userRadius) + 1)
+					+ calculateUpperBound(userLatitude, startLatitude,
+				userRadius);
 		case 5:
 			sectionNum = 5;
-			longitude = startLongitude + generator.nextInt((int) left + 1);
-			latitude = startLatitude + generator.nextInt((int) upper + 1)
-					+ lower;
+			longitude = startLongitude + generator.nextInt((int) calculateLeftBound(userLongitude, startLongitude,
+				userRadius) + 1);
+			latitude = startLatitude + generator.nextInt((int) calculateUpperBound(userLatitude, startLatitude,
+				userRadius) + 1)
+					+ calculateLowerBound(userLatitude, endLatitude,
+				userRadius);
 		case 6:
 			sectionNum = 6;
-			longitude = startLongitude + generator.nextInt((int) left + 1)
-					+ left;
-			latitude = startLatitude + generator.nextInt((int) upper + 1)
-					+ lower;
+			longitude = startLongitude + generator.nextInt((int) calculateLeftBound(userLongitude, startLongitude,
+				userRadius) + 1)
+					+ calculateLeftBound(userLongitude, startLongitude,
+				userRadius);
+			latitude = startLatitude + generator.nextInt((int) calculateUpperBound(userLatitude, startLatitude,
+				userRadius) + 1)
+					+ calculateLowerBound(userLatitude, endLatitude,
+				userRadius);
 		case 7:
 			sectionNum = 7;
-			longitude = startLongitude + generator.nextInt((int) left + 1)
-					+ right;
-			latitude = startLatitude + generator.nextInt((int) upper + 1)
-					+ lower;
+			longitude = startLongitude + generator.nextInt((int) calculateLeftBound(userLongitude, startLongitude,
+				userRadius) + 1)
+					+ calculateRightBound(userLongitude, endLongitude,
+				userRadius);
+			latitude = startLatitude + generator.nextInt((int) calculateUpperBound(userLatitude, startLatitude,
+				userRadius) + 1)
+					+ calculateLowerBound(userLatitude, endLatitude,
+				userRadius);
 		default:
 			break;
 		}
@@ -89,9 +106,9 @@ public class Bones {
 	}
 
 	private double calculateRightBound(double userLongitude,
-			double startLongitude, double userRadius) {
+			double endLongitude, double userRadius) {
 
-		return userLongitude - startLongitude + userRadius;
+		return endLongitude - userLongitude + userRadius;
 	}
 
 	private double calculateUpperBound(double userLatitude,
@@ -101,9 +118,9 @@ public class Bones {
 	}
 
 	private double calculateLowerBound(double userLatitude,
-			double startLatitude, double userRadius) {
+			double endLatitude, double userRadius) {
 
-		return userLatitude - startLatitude - userRadius;
+		return endLatitude - userLatitude  + userRadius;
 	}
 
 	/*public void generateGhost() {
