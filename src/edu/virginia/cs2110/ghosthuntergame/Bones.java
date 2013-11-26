af39581d0e11628ea7b64da8ab5d2f5e513ecd8e
kkramerx <kkramerx@gmail.com>
2013-11-25 20:20:04 -0500

    random

--- src/edu/virginia/cs2110/ghosthuntergame/Bones.java
+++ src/edu/virginia/cs2110/ghosthuntergame/Bones.java
@@ -25,88 +25,90 @@ public class Bones {
 		this.lo= (c-a)*(0.0000034716614);
 		this.la= (d-b)*(0.00000274602523);
 		Random generator = new Random();
-
-		int sectionNum = generator.nextInt(7 + 1);
-		double sect = 8 * Math.random();
-		sectionNum = (int) sect;
-
-		switch (sectionNum) {
-		case 0:
-			sectionNum = 0;
-			longitude = startLongitude
-					+ (lo)/3*Math.random();
-			latitude = startLatitude
-					+ (la)/3*Math.random();
-			break;
-		case 1:
-			sectionNum = 1;
-			longitude = startLongitude
-					+ (lo)/3*(Math.random()+1);
-			latitude = startLatitude
-					+ (la)/3*Math.random();
-			break;
-		case 2:
-			sectionNum = 2;
-			longitude = startLongitude
-					+ (lo)/3*(Math.random()+2);
-			latitude = startLatitude
-					+ (la)/3*Math.random();
-			break;
-		case 3:
-			sectionNum = 3;
-			longitude = startLongitude
-					+ (lo)/3*Math.random();
-			latitude = startLatitude
-					+ (la)/3*(Math.random()+1);
-			break;
+		longitude = lo*Math.random();
+		latitude = la*Math.random();
+		
+//		int sectionNum = generator.nextInt(7 + 1);
+//		double sect = 8 * Math.random();
+//		sectionNum = (int) sect;
+//
+//		switch (sectionNum) {
+//		case 0:
+//			sectionNum = 0;
 //			longitude = startLongitude
-//					+ generator.nextInt((int) calculateLeftBound(userLongitude,
-//							startLongitude, userRadius) + 1);
+//					+ (lo)/3*Math.random();
 //			latitude = startLatitude
-//					+ generator.nextInt((int) calculateUpperBound(userLatitude,
-//							startLatitude, userRadius) + 1)
-//					+ calculateUpperBound(userLatitude, startLatitude,
-//							userRadius);
-			// longitude = startLongitude
-			// + generator.nextInt((int) calculateLeftBound(userLongitude,
-			// startLongitude, userRadius) + 1);
-			// latitude = startLatitude
-			// + generator.nextInt((int) calculateUpperBound(userLatitude,
-			// startLatitude, userRadius) + 1)
-			// + calculateUpperBound(userLatitude, startLatitude,
-			// userRadius);
-
-		case 4:
-			sectionNum = 4;
-			longitude = startLongitude
-					+ (lo)/3*(2+Math.random());
-			latitude = startLatitude
-					+ (la)/3*(1+Math.random());
-			break;
-		case 5:
-			sectionNum = 5;
-			longitude = startLongitude
-			+ (lo)/3*Math.random();
-	latitude = startLatitude
-			+ (la)/3*(Math.random()+2);
-	break;
-		case 6:
-			sectionNum = 6;
-			longitude = startLongitude
-					+ (lo)/3*(1+Math.random());
-			latitude = startLatitude
-					+ (la)/3*(2+Math.random());
-			break;
-		case 7:
-			sectionNum = 7;
-			longitude = startLongitude
-					+ (lo)/3*(2+Math.random());
-			latitude = startLatitude
-					+ (la)/3*(2+Math.random());
-			break;
-		default:
-			break;
-		}
+//					+ (la)/3*Math.random();
+//			break;
+//		case 1:
+//			sectionNum = 1;
+//			longitude = startLongitude
+//					+ (lo)/3*(Math.random()+1);
+//			latitude = startLatitude
+//					+ (la)/3*Math.random();
+//			break;
+//		case 2:
+//			sectionNum = 2;
+//			longitude = startLongitude
+//					+ (lo)/3*(Math.random()+2);
+//			latitude = startLatitude
+//					+ (la)/3*Math.random();
+//			break;
+//		case 3:
+//			sectionNum = 3;
+//			longitude = startLongitude
+//					+ (lo)/3*Math.random();
+//			latitude = startLatitude
+//					+ (la)/3*(Math.random()+1);
+//			break;
+////			longitude = startLongitude
+////					+ generator.nextInt((int) calculateLeftBound(userLongitude,
+////							startLongitude, userRadius) + 1);
+////			latitude = startLatitude
+////					+ generator.nextInt((int) calculateUpperBound(userLatitude,
+////							startLatitude, userRadius) + 1)
+////					+ calculateUpperBound(userLatitude, startLatitude,
+////							userRadius);
+//			// longitude = startLongitude
+//			// + generator.nextInt((int) calculateLeftBound(userLongitude,
+//			// startLongitude, userRadius) + 1);
+//			// latitude = startLatitude
+//			// + generator.nextInt((int) calculateUpperBound(userLatitude,
+//			// startLatitude, userRadius) + 1)
+//			// + calculateUpperBound(userLatitude, startLatitude,
+//			// userRadius);
+//
+//		case 4:
+//			sectionNum = 4;
+//			longitude = startLongitude
+//					+ (lo)/3*(2+Math.random());
+//			latitude = startLatitude
+//					+ (la)/3*(1+Math.random());
+//			break;
+//		case 5:
+//			sectionNum = 5;
+//			longitude = startLongitude
+//			+ (lo)/3*Math.random();
+//	latitude = startLatitude
+//			+ (la)/3*(Math.random()+2);
+//	break;
+//		case 6:
+//			sectionNum = 6;
+//			longitude = startLongitude
+//					+ (lo)/3*(1+Math.random());
+//			latitude = startLatitude
+//					+ (la)/3*(2+Math.random());
+//			break;
+//		case 7:
+//			sectionNum = 7;
+//			longitude = startLongitude
+//					+ (lo)/3*(2+Math.random());
+//			latitude = startLatitude
+//					+ (la)/3*(2+Math.random());
+//			break;
+//		default:
+//			break;
+//		}
 
 		// TODO: still need to physically put an image of the ghost on the map
 	}
