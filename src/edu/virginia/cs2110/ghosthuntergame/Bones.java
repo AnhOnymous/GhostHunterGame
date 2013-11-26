8696b3f6838a371489892e8ec35bd770433edc9a
kkramerx <kkramerx@gmail.com>
2013-11-25 19:56:43 -0500

    bones static 2500

--- src/edu/virginia/cs2110/ghosthuntergame/Bones.java
+++ src/edu/virginia/cs2110/ghosthuntergame/Bones.java
@@ -21,13 +21,10 @@ public class Bones {
 		this.startLongitude = a;
 		this.startLatitude = b;
 		this.endLongitude = c;
-<<<<<<< HEAD
 		this.endLongitude = d;
 		
-=======
 		this.endLatitude = d;
 
->>>>>>> branch 'master' of https://github.com/AnhOnymous/GhostHunterGame.git
 		Random generator = new Random();
 
 		int sectionNum = generator.nextInt(7 + 1);
@@ -38,46 +35,25 @@ public class Bones {
 		case 0:
 			sectionNum = 0;
 			longitude = startLongitude
-<<<<<<< HEAD
 					+ (2500)/3*Math.random();
 			latitude = startLatitude
 					+ (2500)/3*Math.random();
-=======
-					+ (this.endLongitude - this.startLongitude) / 3
-					* Math.random();
-			latitude = startLatitude + (this.endLatitude - this.startLatitude)
-					/ 3 * Math.random();
->>>>>>> branch 'master' of https://github.com/AnhOnymous/GhostHunterGame.git
 		case 1:
 			sectionNum = 1;
 			longitude = startLongitude
-<<<<<<< HEAD
 					+ (2500)/3*(Math.random()+1);
 			latitude = startLatitude
 					+ (2500)/3*Math.random();
-=======
-					+ (this.endLongitude - this.startLongitude) / 3
-					* (Math.random() + 1);
-			latitude = startLatitude + (this.endLatitude - this.startLatitude)
-					/ 3 * Math.random();
->>>>>>> branch 'master' of https://github.com/AnhOnymous/GhostHunterGame.git
 		case 2:
 			sectionNum = 2;
 			longitude = startLongitude
-<<<<<<< HEAD
 					+ (2500)/3*(Math.random()+2);
 			latitude = startLatitude
 					+ (2500)/3*Math.random();
-=======
-					+ (this.endLongitude - this.startLongitude) / 3
-					* (Math.random() + 2);
-			latitude = startLatitude + (this.endLatitude - this.startLatitude)
-					/ 3 * Math.random();
->>>>>>> branch 'master' of https://github.com/AnhOnymous/GhostHunterGame.git
+
 		case 3:
 			sectionNum = 3;
 			longitude = startLongitude
-<<<<<<< HEAD
 					+ (2500)/3*Math.random();
 			latitude = startLatitude
 					+ (2500)/3*(Math.random()+1);
@@ -89,11 +65,6 @@ public class Bones {
 //							startLatitude, userRadius) + 1)
 //					+ calculateUpperBound(userLatitude, startLatitude,
 //							userRadius);
-=======
-					+ (this.endLongitude - this.startLongitude) / 3
-					* Math.random();
-			latitude = startLatitude + (this.endLatitude - this.startLatitude)
-					/ 3 * (Math.random() + 1);
 			// longitude = startLongitude
 			// + generator.nextInt((int) calculateLeftBound(userLongitude,
 			// startLongitude, userRadius) + 1);
@@ -102,59 +73,31 @@ public class Bones {
 			// startLatitude, userRadius) + 1)
 			// + calculateUpperBound(userLatitude, startLatitude,
 			// userRadius);
->>>>>>> branch 'master' of https://github.com/AnhOnymous/GhostHunterGame.git
+
 		case 4:
 			sectionNum = 4;
 			longitude = startLongitude
-<<<<<<< HEAD
 					+ (2500)/3*(2+Math.random());
 			latitude = startLatitude
 					+ (2500)/3*(1+Math.random());
-=======
-					+ (this.endLongitude - this.startLongitude) / 3
-					* (2 + Math.random());
-			latitude = startLatitude + (this.endLatitude - this.startLatitude)
-					/ 3 * (1 + Math.random());
->>>>>>> branch 'master' of https://github.com/AnhOnymous/GhostHunterGame.git
 		case 5:
 			sectionNum = 5;
 			longitude = startLongitude
-<<<<<<< HEAD
 			+ (2500)/3*Math.random();
 	latitude = startLatitude
 			+ (2500)/3*(Math.random()+2);
-=======
-					+ (this.endLongitude - this.startLongitude) / 3
-					* Math.random();
-			latitude = startLatitude + (this.endLatitude - this.startLatitude)
-					/ 3 * (Math.random() + 2);
->>>>>>> branch 'master' of https://github.com/AnhOnymous/GhostHunterGame.git
 		case 6:
 			sectionNum = 6;
 			longitude = startLongitude
-<<<<<<< HEAD
 					+ (2500)/3*(1+Math.random());
 			latitude = startLatitude
 					+ (2500)/3*(2+Math.random());
-=======
-					+ (this.endLongitude - this.startLongitude) / 3
-					* (1 + Math.random());
-			latitude = startLatitude + (this.endLatitude - this.startLatitude)
-					/ 3 * (2 + Math.random());
->>>>>>> branch 'master' of https://github.com/AnhOnymous/GhostHunterGame.git
 		case 7:
 			sectionNum = 7;
 			longitude = startLongitude
-<<<<<<< HEAD
 					+ (2500)/3*(2+Math.random());
 			latitude = startLatitude
 					+ (2500)/3*(2+Math.random());
-=======
-					+ (this.endLongitude - this.startLongitude) / 3
-					* (2 + Math.random());
-			latitude = startLatitude + (this.endLatitude - this.startLatitude)
-					/ 3 * (2 + Math.random());
->>>>>>> branch 'master' of https://github.com/AnhOnymous/GhostHunterGame.git
 		default:
 			break;
 		}
